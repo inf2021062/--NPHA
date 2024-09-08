10e25011f24f68f635342810e8a81774c70687c3
inf2021122 <115413889+inf2021122@users.noreply.github.com>
2024-09-08 14:24:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,11 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.cluster import MeanShift, AffinityPropagation
+from sklearn.feature_selection import SelectKBest, f_classif
+from mpl_toolkits.mplot3d import Axes3D
 st.title("NPHA")
 
-tabs = ["Data Overview", "Machine Learning", "2D Visualization", "Info"]
+tabs = ["Data Overview", "Machine Learning", "2D/3D Visualization", "Info"]
 selected_tab = st.radio("choose tab", tabs )
 
 if selected_tab == "Data Overview":
@@ -42,7 +43,7 @@ elif selected_tab == "Machine Learning":
           data = pd.concat([data.drop(categorical_cols, axis=1), encoded_cols], axis=1)
 
           st.write("choose category of the algorithms of Machine Learning")
-          method = st.radio ("Algorithms", ("Classification Algorithms", "Clustering Algorithms"))
+          method = st.radio ("Algorithms", ("Classification Algorithms", "Feature Selection"))
 
 
 
@@ -72,25 +73,22 @@ elif selected_tab == "Machine Learning":
              st.write(f"the accuracy of the classifier random forest is: {rf_accuracy}")
              st.write(f"the accuracy of the classifier MLP is: {ml_accuracy}")
 
-          elif method == "Clustering Algorithms":
-              clustering_algorithm = st.selectbox("choose clustering algorithm", ("Mean Shift", "Affinity Propagation"))
+          elif method == "Feature Selection":
+              st.write("Select the best features")
 
-              if clustering_algorithm == "Mean Shift":
-                  mean_shift = MeanShift()
-                  labels = mean_shift.fit_predict(data)
-                  st.write("Cluster labels:")
-                  st.write(labels)
+              X = data.iloc[:, :-1]
+              y = data.iloc[:,  -1]
 
-              elif clustering_algorithm == "Affinity Propagation":
-                  affinity_propagation = AffinityPropagation()
-                  labels = affinity_propagation.fit_predict(data)
-                  st.write("cluster labels:") 
-                  st.write(labels) 
+              k = st.slider("Select the number of features", 1, X.shape[1])
+              selector = SelectKBest(score_func=f_classif, k=k)
+              X_new = selector.fit_transform(X, y)
 
-
+              st.write("Shape of data after feature selection:", X_new.shape)
+              st.write("Selected features scores:")
+              st.write(selector.scores_)
              
 
-elif selected_tab == "2D Visualization":
+elif selected_tab == "2D/3D Visualization":
     uploaded_file = st.file_uploader("upload file in format CSV,TXT (no_header)", type=["csv","txt"])
          
     
@@ -105,18 +103,30 @@ elif selected_tab == "2D Visualization":
         data = pd.concat([data.drop(categorical_cols, axis=1), encoded_cols], axis=1)
 
         st.write("Select Reduction Dimension Algorithm:")
-        reduction_algorithm = st.selectbox("Dimensonality Reduction", ("PCA", "t-SNE"))
+        reduction_algorithm = st.selectbox("Dimensonality Reduction", ("PCA 2D", "PCA 3D", "t-SNE"))
 
-        if reduction_algorithm == "PCA":
+        if reduction_algorithm == "PCA 2D":
             pca = PCA(n_components=2)
             X_reduced = pca.fit_transform(data)
-            plt.scatter(X_reduced[:, 0], X_reduced[:, 1])
+            plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c=X_reduced[:, 0], cmap='viridis')
+            plt.colorbar()
             st.pyplot()
+
+        elif reduction_algorithm == "PCA 3D":
+            pca = PCA(n_components=3)
+            X_reduced = pca.fit_transform(data)
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+            scatter = ax.scatter(X_reduced[:, 0], X_reduced[:, 1], X_reduced[:, 2], c=X_reduced[:, 0], cmap='plasma')
+            fig.colorbar(scatter)
+            st.pyplot()
+
 
         elif reduction_algorithm == "t-SNE":
              tsne = TSNE(n_components=2)
              X_reduced = tsne.fit_transform(data)
-             plt.scatter(X_reduced[:, 0], X_reduced[:, 1])
+             plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c=X_reduced [:, 0], cmap='inferno')
+             plt.colorbar()
              st.pyplot()
 
 elif selected_tab == "Info":
@@ -125,21 +135,14 @@ elif selected_tab == "Info":
 
     st.write("## How it works")
     st.write("1. **Data Overview**: Upload your dataset and view its content.")
-    st.write("2. **Machine Learning**: Choose and set machine learning algorithms for clastering and classification.")
-    st.write("3. **2D Visualitation**: Visualize your data PCA or t-SNE for dimentionality reduction.")
+    st.write("2. **Machine Learning**: Choose and set machine learning algorithms for feature selection and classification.")
+    st.write("3. **2D/3D Visualitation**: Visualize your data PCA or t-SNE for dimentionality reduction.")
 
     st.write("## Development Team")
     st.write("This app developed by a team of students from department of informatics from ionian university.")
     st.write("- **Tilemahos Theodoridis**: Developed the data upload and overview functionality and developed the machine learning algorithm integration.")
-    st.write("- **Giorgos Liontos**: Created the 2D visualization features and design the user interface and handled the EDA plots.")
+    st.write("- **Giorgos Liontos**: Created the 2D/3D visualization features and design the user interface and handled the EDA plots.")
 
     st.write("## Specific Tasks")
-    st.write("- **Tilemahos Theodoridis**: Data upload and overview, PCA and t-SNE integration, also integration of meanshift and affinity propagation algorithms.")
+    st.write("- **Tilemahos Theodoridis**: Data upload and overview, PCA and t-SNE integration, also integration of feature selection algorithms.")
     st.write("- **Giorgos Liontos**: Implementation of classification algorithms and accuracy metrics, design and layaout of application, creation of the info tab.")
-
-
-
-
-
-    
-
